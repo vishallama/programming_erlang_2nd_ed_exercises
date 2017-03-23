e7fe1b5c56bfc7c0ff5e0b9db4834559702c9a86
Vishal Lama <vishal.lama@gmail.com>
2017-03-23 08:43:39 -0600

    Implement odds_and_evens2/1

--- src/04_modules_and_functions/lib_misc.erl
+++ src/04_modules_and_functions/lib_misc.erl
@@ -10,7 +10,7 @@
 -author("vishal").
 
 %% API
--export([for/3, qsort/1, pythag/1, perms/1]).
+-export([for/3, qsort/1, pythag/1, perms/1, odds_and_evens2/1]).
 
 for(Max, Max, F) ->
   [F(Max)];
@@ -37,3 +37,14 @@ perms([]) ->
   [[]];
 perms(L) ->
   [[H | T] || H <- L, T <- perms(L -- [H])].
+
+odds_and_evens2(L) ->
+  odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H | T], Odds, Evens) ->
+  case (H rem 2) of
+    1 -> odds_and_evens_acc(T, [H | Odds], Evens);
+    0 -> odds_and_evens_acc(T, Odds, [H | Evens])
+  end;
+odds_and_evens_acc([], Odds, Evens) ->
+  {lists:reverse(Odds), lists:reverse(Evens)}.

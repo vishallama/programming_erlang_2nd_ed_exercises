9446660b52b59f8825de618f123e94727782cbfc
Vishal Lama <vishal.lama@gmail.com>
2017-03-23 08:37:05 -0600

    Implement perimeter/1

--- src/04_modules_and_functions/geometry.erl
+++ src/04_modules_and_functions/geometry.erl
@@ -10,7 +10,7 @@
 -author("vishal").
 
 %% API
--export([area/1]).
+-export([area/1, perimeter/1]).
 
 area({rectangle, Width, Height}) ->
   Width * Height;
@@ -20,3 +20,13 @@ area({circle, Radius}) ->
   math:pi() * Radius * Radius;
 area({right_triangle, Base, Height}) ->
   Base * Height / 2.
+
+perimeter({rectangle, Width, Height}) ->
+  2 * (Width + Height);
+perimeter({square, Side}) ->
+  4 * Side;
+perimeter({circle, Radius}) ->
+  2 * math:pi() * Radius;
+perimeter({right_triangle, Base, Height}) ->
+  Hypot = math:sqrt(Base * Base + Height * Height),
+  Base + Height + Hypot.

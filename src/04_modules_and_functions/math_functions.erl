a5cd1a24c60651538c479d6d08e359bab0f3a2b6
Vishal Lama <vishal.lama@gmail.com>
2017-03-23 11:09:12 -0600

    Implement even/1 and odd/1

---
+++ src/04_modules_and_functions/math_functions.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author vishal
+%%% @copyright (C) 2017
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Mar 2017 11:05 AM
+%%%-------------------------------------------------------------------
+-module(math_functions).
+-author("vishal").
+
+%% API
+-export([even/1, odd/1]).
+
+even(X) when is_integer(X) ->
+  X rem 2 =:= 0.
+
+odd(X) when is_integer(X) ->
+  X rem 2 =:= 1.

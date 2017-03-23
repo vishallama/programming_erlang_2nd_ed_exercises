b71e29b050175b8cb640cecb71e2b210918d7cf6
Vishal Lama <vishal.lama@gmail.com>
2017-03-23 11:23:51 -0600

    Implement filter/2

--- src/04_modules_and_functions/math_functions.erl
+++ src/04_modules_and_functions/math_functions.erl
@@ -10,10 +10,13 @@
 -author("vishal").
 
 %% API
--export([even/1, odd/1]).
+-export([even/1, odd/1, filter/2]).
 
 even(X) when is_integer(X) ->
   X rem 2 =:= 0.
 
 odd(X) when is_integer(X) ->
   X rem 2 =:= 1.
+
+filter(F, L) ->
+  [X || X <- L, F(X)].

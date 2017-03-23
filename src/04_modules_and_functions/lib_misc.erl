9479ce36dd31dcd3e86d2a17b9835de32ac5d713
Vishal Lama <vishal.lama@gmail.com>
2017-03-22 23:05:00 -0600

    Implement qsort/1 and pythag/1

--- src/04_modules_and_functions/lib_misc.erl
+++ src/04_modules_and_functions/lib_misc.erl
@@ -10,9 +10,25 @@
 -author("vishal").
 
 %% API
--export([for/3]).
+-export([for/3, qsort/1, pythag/1]).
 
 for(Max, Max, F) ->
   [F(Max)];
 for(I, Max, F) when I < Max ->
   [F(I) | for(I + 1, Max, F)].
+
+qsort([]) ->
+  [];
+qsort([Pivot | T]) ->
+  qsort([X || X <- T, X < Pivot])
+  ++ [Pivot] ++
+  qsort([X || X <- T, X >= Pivot]).
+
+pythag(N) ->
+  [ {A, B, C} ||
+    A <- lists:seq(1, N),
+    B <- lists:seq(1, N),
+    C <- lists:seq(1, N),
+    A + B + C =< N,
+    A * A + B * B =:= C * C
+  ].

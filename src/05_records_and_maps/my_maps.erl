5977cca2f18332a87d4c7c841375faedea1564b3
Vishal Lama <vishal.lama@gmail.com>
2017-03-24 10:18:50 -0600

    Implement map_search_pred/2

--- src/05_records_and_maps/my_maps.erl
+++ src/05_records_and_maps/my_maps.erl
@@ -10,7 +10,7 @@
 -author("vishal").
 
 %% API
--export([count_characters/1]).
+-export([count_characters/1, map_search_pred/2]).
 
 count_characters(Str) ->
   count_characters(Str, #{}).
@@ -19,3 +19,15 @@ count_characters([H | T], X) ->
   count_characters(T, maps:put(H, maps:get(H, X, 0) + 1, X));
 count_characters([], X) ->
   X.
+
+map_search_pred(Map, Pred) ->
+  map_search_pred(Map, Pred, maps:keys(Map)).
+
+map_search_pred(_Map, _Pred, []) ->
+  error("no match");
+map_search_pred(Map, Pred, [Key | T]) ->
+  Value = maps:get(Key, Map),
+  case Pred(Key, Value) of
+    true -> {Key, Value};
+    false -> map_search_pred(Map, Pred, T)
+  end.

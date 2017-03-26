ac14a341bb5bfd0795b21969403c4711e468f730
Vishal Lama <vishal.lama@gmail.com>
2017-03-26 15:08:32 -0600

    Add module walks to illustrate types in Erlang

---
+++ src/09_types/walks.erl
@@ -0,0 +1,9 @@
+-module(walks).
+-export([plan_route/2]).
+
+-spec plan_route(From :: point(), To :: point()) -> route().
+
+-type direction() :: north | south | east | west.
+-type point() :: {integer(), integer()}.
+-type route() :: [{go, direction(), integer()}].
+
